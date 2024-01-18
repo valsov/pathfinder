34010256ab41df4f9135e9eb6259ded4c1aa7e04
valsov <valentin.sovignet@sfr.fr>
2024-01-18 22:11:44 +0100

    Introduce graph structures

--- pathfinder/src/main.rs
+++ pathfinder/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // Create graph
+
+    // Compute best path
+}
+
+struct Graph {
+    nodes: Vec<Node>,
+    paths: Vec<Path>,
+}
+
+struct Node {
+    name: &'static str,
+}
+
+struct Path {
+    from: &'static str,
+    to: &'static str,
+    weight: usize,
 }
